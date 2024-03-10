7261a5e142db6dd482f0a3319ac9a8a767afb697
rayantahtouh <153671884+rayantahtouh@users.noreply.github.com>
2024-03-10 22:49:12 +0000

    Update award.py

--- award.py
+++ award.py
@@ -1,20 +1,20 @@
 #Assing each competition a time using float
-t_1=float(input("Please give me how many minutes it took you to finish swimming:"))
-t_2=float(input("Please give me how many minutes it took you to finish cycling: "))
-t_3=float(input("Please give me how many minutes it took you to finish running: " ))
+time_1=float(input("Please give me how many minutes it took you to finish swimming:"))
+time_2=float(input("Please give me how many minutes it took you to finish cycling: "))
+time_3=float(input("Please give me how many minutes it took you to finish running: " ))
 
 #Calculate and display the total time for the thriatlon
-t=t_1+t_2+t_3
-print(f"The total time taken to complete the triathlon is: {t}")
+time=time_1+time_2+time_3
+print(f"The total time taken to complete the triathlon is: {time}")
 
 #Condition that determines which award category 
-if(0<t<=100):
+if(0<time<=100):
     print("The award that the participant will receive is: Provincial Colours")
 
-elif(100<t<=105):
+elif(100<time<=105):
     print("The award that the participant will receive is: Provincial Half Colours")
 
-elif(106<=t<=110):
+elif(106<=time<=110):
     print("The award that the participant will receive is: Provincial Scroll")
 
 else:
